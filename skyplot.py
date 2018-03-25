f89030739fc3c18345f62b373a66edae7952c47f
Abhishek Singla <singla.abhishek95@gmail.com>
2018-03-26 00:59:59 +0530

    Major Changes

--- skyplot.py
+++ skyplot.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import seaborn as sns
 
-def boxplot(x, y, data, texttype='count'):
+def boxplot(x, y, data, texttype='count', showdatapts=False, includemissing=True):
     
     categorical = data.copy()
-    for i in categorical.columns:
-        if categorical[i].isnull().any():
-            categorical[i].fillna('MISSING', inplace=True)
+    
+    if includemissing==True:
+        for i in categorical.columns:
+            if categorical[i].isnull().any():
+                categorical[i].fillna('MISSING', inplace=True)
 
 
     def plot(x, y, **kwargs):
@@ -14,15 +16,30 @@ def boxplot(x, y, data, texttype='count'):
         
         df = pd.DataFrame(dict(Value=x, Target=y))
         df.sort_values(by='Value', inplace=True)
-        ax = sns.boxplot(x=Value, y=Target,data=df)
+        
+        # Plotting boxplot
+        ax = sns.boxplot(x='Value', y='Target',data=df)
         ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
-        pos_y = df.Value.value_counts().sort_index().values
+        
+        # Showing data points incase 'showdatapts' is TRUE
+        if showdatapts == True:
+            ax = sns.swarmplot(x='Value', y='Target',data=df, color='0.25', size=8)
+        
+        # Defining text type
+        if texttype=='count':
+            pos_y = df.Value.value_counts().sort_index().values
+        elif textype=='median':
+            pos_y = df.groupby('Value').Target.median().values
+        elif textype=='mean':
+            pos_y = df.groupby('Value').Target.mean().values
+            
         text = [str(i) for i in pos_y]
         pos_x = df.Value.unique()
-
+        
+        # Print info of dimension on x-axis
         for tick, label in zip(range(len(text)), text):
 
-            ax.text(tick, pos_y[tick], label, weight='bold', fontsize=10, color='k', horizontalalignment='center')
+            ax.text(tick, 0, label, weight='bold', fontsize=10, color='k', horizontalalignment='center')
 
     f = pd.melt(categorical,
             id_vars=y, 
@@ -30,6 +47,6 @@ def boxplot(x, y, data, texttype='count'):
             value_vars=x
                )
 
-    g = sns.FacetGrid(f, col="variable", col_wrap=3, sharex=False, sharey=True, size=7)
+    g = sns.FacetGrid(f, col="variable", col_wrap=2, sharex=False, sharey=True, size=7)
     g = g.map(plot, "value", y)
     
